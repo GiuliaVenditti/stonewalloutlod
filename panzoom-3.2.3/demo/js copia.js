1bfe420758a071d7be3055a976ced64fbfc400ee
GiuliaVenditti <72979010+GiuliaVenditti@users.noreply.github.com>
2021-04-01 05:08:54 +0200

    Add files via upload

---
+++ panzoom-3.2.3/demo/js copia.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+
+
+$(document).ready(function(){
+  $(".change").click(function(){
+  $(".hide").toggle();
+});
+});
+
+$(document).ready(function(){
+  $(".change").click(function(){
+  $(".show").toggle();
+});
+});
