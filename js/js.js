099bc3b31a9913b54b4c3b2c000d65925aab94d8
GiuliaVenditti <72979010+GiuliaVenditti@users.noreply.github.com>
2021-04-01 04:13:37 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,89 @@
+// JavaScript Document
+
+// Hide Header on on scroll down
+
+
+
+// Cards
+
+$(document).ready(function(){
+  $(".uno").hover(function(){
+    $(".one").show();
+    }, function(){
+    $(".one").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".due").hover(function(){
+    $(".two").show();
+    }, function(){
+    $(".two").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".tre").hover(function(){
+    $(".three").show();
+    }, function(){
+    $(".three").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".quattro").hover(function(){
+    $(".four").show();
+    }, function(){
+    $(".four").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".cinque").hover(function(){
+    $(".five").show();
+    }, function(){
+    $(".five").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".sei").hover(function(){
+    $(".six").show();
+    }, function(){
+    $(".six").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".sette").hover(function(){
+    $(".seven").show();
+    }, function(){
+    $(".seven").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".otto").hover(function(){
+    $(".eight").show();
+    }, function(){
+    $(".eight").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".nove").hover(function(){
+    $(".nine").show();
+    }, function(){
+    $(".nine").hide();
+  });
+});
+
+$(document).ready(function(){
+  $(".dieci").hover(function(){
+    $(".ten").show();
+    }, function(){
+    $(".ten").hide();
+  });
+});
+
+
